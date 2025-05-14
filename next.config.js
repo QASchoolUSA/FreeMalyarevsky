1435eb6d52d5de38fe2f0b05ea6d889e805db4c6
Nick Kedrov <64873838+QASchoolUSA@users.noreply.github.com>
2025-05-14 02:26:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
